911403cb8900a1118afeab74e31453ebfd33985e
Sher Her <sherher21@gmail.com>
2022-06-07 20:15:45 -0700

    updated enumerable methods

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,48 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    index = 0
+    my_each do |value|
+      yield(value, index)
+      index += 1
+    end
+  end
+
+  def my_select
+    array = []
+    my_each { |value| array << value if yield(value) }
+    array
+  end
+
+  def my_all?
+    valid = true
+    my_each { |value| valid = false unless yield(value) }
+    valid
+  end
+
+  def my_none?
+    valid = true
+    my_each { |value| valid = false if yield(value) }
+    valid
+  end
+
+  def my_count
+    return length unless block_given?
+    count = 0
+    my_each { |value| count += 1 if yield(value) }
+    count
+  end
+
+  def my_map
+    array = []
+    my_each { |value| array << yield(value) }
+    array
+  end
+
+  def my_inject(initial_value)
+    my_each { |value| initial_value = yield(initial_value, value)}
+    initial_value
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +51,12 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    array = []
+    for i in 0..length - 1
+      yield at(i)
+      array << at(i)
+    end
+    array
+  end
 end
